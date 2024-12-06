a29694e2496b355aba48c0d1582b538abda0deaa
Tyr <langtuananh2424@gmail.com>
2024-12-06 21:31:00 +0700

    Thêm các tệp mới vào repository

---
+++ controllers/AuthColtroller.php
@@ -0,0 +1,8 @@
+<?php
+class AuthColtroller {
+    public function login($username, $password) {}
+    public function register($username, $password) {}
+    public function logout() {}
+    public function isLoggedIn() {}
+    public function isLoggedOut() {}
+}
